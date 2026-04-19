fe91f2a9823eaac5e342b830dc828a3b023b44b1
Alex <jex@vivobook.localdomain>
2026-04-19 16:36:25 +0300

    Update fenwick

--- extra/fenwick.hpp
+++ extra/fenwick.hpp
@@ -1,52 +1,73 @@
 #pragma once
 
-#include <vector>
 #include <cstddef>
+#include <vector>
 
 namespace fenwick_tree {
 
-template <typename T>
+template <typename T, std::size_t Dims = 1>
+    requires(Dims > 1)
 class [[nodiscard]] FenwickTree {
+private:
+    using SubTree = FenwickTree<T, Dims - 1>;
+    std::vector<SubTree> tree_{};
+
 public:
-    explicit FenwickTree(std::size_t size) : tree_(size + 1, T()) {
+    template <typename... Args>
+    explicit FenwickTree(std::size_t size, Args&&... args)
+        : tree_(size + 1, SubTree(std::forward<Args>(args)...)) {
     }
 
-    [[nodiscard]] T Query(std::size_t index) const {
-        ++index;
-        T result = T();
-        while (index > 0) {
-            result += tree_[index];
-            index -= LSB(index);
+    template <typename... Args>
+    void Add(std::size_t index, Args&&... args) {
+        for (++index; index < tree_.size(); index += LSB(index)) {
+            tree_[index].Add(std::forward<Args>(args)...);
+        }
+    }
+
+    template <typename... Args>
+    [[nodiscard]] T PrefixSum(std::size_t index, Args&&... args) const {
+        T result{};
+        for (++index; index > 0; index -= LSB(index)) {
+            result += tree_[index].PrefixSum(std::forward<Args>(args)...);
         }
         return result;
     }
 
-    [[nodiscard]] T Query(std::size_t left, std::size_t right) const {
-        if (left > right) {
-            return T();
-        }
+private:
+    [[nodiscard]] constexpr static std::size_t LSB(std::size_t x) noexcept {
+        return x & (~x + 1);
+    }
+};
 
-        auto res = Query(right);
-        if (left > 0) {
-            res -= Query(left - 1);
-        }
-        return res;
+template <typename T>
+class [[nodiscard]] FenwickTree<T, 1> {
+private:
+    std::vector<T> tree_{};
+
+public:
+    explicit FenwickTree(std::size_t size) : tree_(size + 1, T{}) {
     }
 
-    void Update(std::size_t index, const T& value) {
-        std::size_t i = index + 1;
-        while (i < tree_.size()) {
-            tree_[i] += value;
-            i += LSB(i);
+    template <typename U>
+    void Add(std::size_t index, U&& value) {
+        for (++index; index < tree_.size(); index += LSB(index)) {
+            tree_[index] += std::forward<U>(value);
         }
+    }
+
+    [[nodiscard]] T PrefixSum(std::size_t index) const {
+        T result{};
+        for (++index; index > 0; index -= LSB(index)) {
+            result += tree_[index];
+        }
+        return result;
     }
 
 private:
-    [[nodiscard]] static std::size_t LSB(std::size_t index) noexcept {
-        return index & (~index + 1);
+    [[nodiscard]] constexpr static std::size_t LSB(std::size_t x) noexcept {
+        return x & (~x + 1);
     }
-
-    std::vector<T> tree_;
 };
 
 }  // namespace fenwick_tree

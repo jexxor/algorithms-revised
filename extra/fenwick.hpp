0641c1ad76929b128e1133fb086c2b79346dfaaa
Alex <jex@vivobook.localdomain>
2026-04-19 17:53:26 +0300

    Update fenwick

--- extra/fenwick.hpp
+++ extra/fenwick.hpp
@@ -7,9 +7,8 @@
 namespace fenwick_tree {
 
 template <typename T, std::size_t Dims>
+    requires(Dims > 0 && std::is_arithmetic_v<T>)
 class [[nodiscard]] FenwickTree {
-    static_assert(Dims > 0, "Fenwick tree must have at least one dimension");
-
 public:
     explicit FenwickTree(const std::array<std::size_t, Dims>& sizes) : sizes_(sizes) {
         std::size_t total_size = 1;
@@ -18,38 +17,36 @@ public:
         }
 
         tree_.assign(total_size, T{});
-        strides_[Dims - 1] = 1;
+        strides_.back() = 1;
         for (std::size_t i = Dims - 1; i > 0; --i) {
             strides_[i - 1] = strides_[i] * (sizes_[i] + 1);
         }
     }
 
     template <typename... Args>
-    void Update(const T& delta, Args... args) {
-        static_assert(sizeof...(Args) == Dims,
-                      "Number of indices must match the number of dimensions");
-        Update({static_cast<std::size_t>(args)...}, delta);
-    }
-
-    void Update(std::array<std::size_t, Dims> indices, const T& delta) {
-        for (auto& index : indices) {
-            ++index;
-        }
-        RecursiveUpdate(0, 0, indices, delta);
-    }
-
-    template <typename... Args>
     [[nodiscard]] T Query(Args... args) const {
-        static_assert(sizeof...(Args) == Dims,
-                      "Number of indices must match the number of dimensions");
+        static_assert(sizeof...(Args) == Dims, "Number of arguments must match dimensions");
         return Query({static_cast<std::size_t>(args)...});
     }
 
-    [[nodiscard]] T Query(std::array<std::size_t, Dims> indices) const {
-        for (auto& index : indices) {
+    template <typename... Args>
+    void Update(const T& delta, Args... args) {
+        static_assert(sizeof...(Args) == Dims, "Number of arguments must match dimensions");
+        Update({static_cast<std::size_t>(args)...}, delta);
+    }
+
+    [[nodiscard]] T Query(std::array<std::size_t, Dims> coords) const {
+        for (auto& index : coords) {
             ++index;
         }
-        return RecursiveQuery(0, 0, indices);
+        return QueryImpl<0>(0, coords);
+    }
+
+    void Update(std::array<std::size_t, Dims> coords, const T& delta) {
+        for (auto& index : coords) {
+            ++index;
+        }
+        UpdateImpl<0>(0, coords, delta);
     }
 
 private:
@@ -57,29 +54,35 @@ private:
         return x & (~x + 1);
     }
 
-    T RecursiveQuery(std::size_t dims, std::size_t offset,
-                     std::array<std::size_t, Dims>& coords) const {
-        if (dims == Dims) {
-            return tree_[offset];
-        }
+    template <std::size_t Dim>
+    void UpdateImpl(std::size_t offset, const std::array<std::size_t, Dims>& coords,
+                    const T& delta) {
+        if constexpr (Dim == Dims) {
+            tree_[offset] += delta;
+        } else {
+            const std::size_t stride = strides_[Dim];
+            const std::size_t index = coords[Dim];
 
-        T result{};
-        for (std::size_t i = coords[dims]; i > 0; i -= LSB(i)) {
-            result += RecursiveQuery(dims + 1, offset + strides_[dims] * i, coords);
+            for (std::size_t i = index; i <= sizes_[Dim]; i += LSB(i)) {
+                UpdateImpl<Dim + 1>(offset + i * stride, coords, delta);
+            }
         }
-
-        return result;
     }
 
-    void RecursiveUpdate(std::size_t dims, std::size_t offset,
-                         std::array<std::size_t, Dims>& coords, const T& delta) {
-        if (dims == Dims) {
-            tree_[offset] += delta;
-            return;
-        }
+    template <std::size_t Dim>
+    [[nodiscard]] T QueryImpl(std::size_t offset,
+                              const std::array<std::size_t, Dims>& coords) const {
+        if constexpr (Dim == Dims) {
+            return tree_[offset];
+        } else {
+            T result{};
+            const std::size_t stride = strides_[Dim];
 
-        for (std::size_t i = coords[dims]; i < sizes_[dims] + 1; i += LSB(i)) {
-            RecursiveUpdate(dims + 1, offset + strides_[dims] * i, coords, delta);
+            for (std::size_t i = coords[Dim]; i > 0; i -= LSB(i)) {
+                result += QueryImpl<Dim + 1>(offset + i * stride, coords);
+            }
+
+            return result;
         }
     }
 
